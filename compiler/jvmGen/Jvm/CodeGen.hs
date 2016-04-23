ddc6d22072aedb83c026bd82bd57bcbc830610d2
Thomas Jakway <tjakway@nyu.edu>
2016-04-23 13:51:38 -0400

    added skeleton method compileInstruction for translating from expression -> concrete operations

--- compiler/jvmGen/Jvm/CodeGen.hs
+++ compiler/jvmGen/Jvm/CodeGen.hs
@@ -1,6 +1,12 @@
 module Jvm.CodeGen where
 
 import Jvm.Types
+import Jvm.Expressions
+import Jvm.Instructions
 
 stackSize :: JVMMethod -> Int
 stackSize = undefined
+
+-- | compile an abstract instruction into its concrete type
+compileInstruction :: AbstractInstruction -> Instruction
+compileInstruction = undefined
