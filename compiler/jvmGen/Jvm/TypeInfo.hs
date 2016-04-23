79bfae287ebff839784e851924ee9e7e94b47f94
Thomas Jakway <tjakway@nyu.edu>
2016-04-23 15:07:57 -0400

    added stub methodSignature

--- compiler/jvmGen/Jvm/TypeInfo.hs
+++ compiler/jvmGen/Jvm/TypeInfo.hs
@@ -1,9 +1,13 @@
 module Jvm.TypeInfo where
 
 import Jvm.Types
+import FastString
 
 -- only variables can be constant
 -- methods can be declared final but this has a totally different meaning
 isConstant :: JVMVar -> Bool
 isConstant (JVMVar _ []) = False
 isConstant (JVMVar _ attrs) = Final `elem` attrs
+
+methodSignature :: JVMClass -> JVMMethod -> FastString
+methodSignature = undefined
