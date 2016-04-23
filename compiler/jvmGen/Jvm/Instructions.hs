1928537d6540175bbe10e430f7b35b3951e254bd
Thomas Jakway <tjakway@nyu.edu>
2016-04-23 16:30:47 -0400

    added instructions

--- compiler/jvmGen/Jvm/Instructions.hs
+++ compiler/jvmGen/Jvm/Instructions.hs
@@ -5,7 +5,10 @@ module Jvm.Instructions where
 #include "HsVersions.h"
 
 data Instruction
+    -- return to the address in the passed local variable
     = Ret VarNum
+
+    -- loads and stores for various types
     | Aload VarNum
     | Astore VarNum
     | Dload VarNum
@@ -16,3 +19,45 @@ data Instruction
     | Istore VarNum
     | Lload VarNum
     | Lstore VarNum
+
+    -- the following instructions push integer constants on the stack and take
+    -- no operands
+    | Iconst_m1 -- ^ push -1
+    | Iconst_0  -- ^ push 0
+    | Iconst_1  -- and so forth
+    | Iconst_2
+    | Iconst_3
+    | Iconst_4
+    | Iconst_5
+    -- same as above with floats
+    | Fconst_0
+    | Fconst_1
+    | Fconst_2
+    -- longs (these are wide)
+    | Lconst_0
+    | Lconst_1
+    -- doubles (also wide)
+    | Dconst_0
+    | Dconst_1
+    -- pushes a null reference
+    | Aconst_null
+    
+    -- method calls
+    -- the MethodSpec indicates which method to call
+    | Invokenonvirtual MethodSpec
+    | Invokestate MethodSpec
+    | Invokenonvirtual MethodSpec
+
+    -- instructions to manipulate fields
+    -- see FieldSpec and FieldDescriptor for an explanation of their
+    -- meanings
+    | Getfield FieldSpec FieldDescriptor
+    | Getstatic FieldSpec FieldDescriptor
+    | Putfield FieldSpec FieldDescriptor
+    | Putstatic FieldSpec FieldDescriptor
+
+    | Newarray FastString
+
+    -- "load constant", pushes a value on the stack
+    -- Jasmin automatically handles converting ldc <=> ldc_w
+    | Ldc JVMValue
