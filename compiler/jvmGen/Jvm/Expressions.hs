faabfceb8e8a3c74070c27085f312e4d2f785f07
Thomas Jakway <tjakway@nyu.edu>
2016-04-23 13:51:07 -0400

    abstract expression types

---
+++ compiler/jvmGen/Jvm/Expressions.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE CPP #-}
+
+module Jvm.Expressions where
+
+#include "HsVersions.h"
+
+-- | a higher level of abstraction than raw JVM instructions
+-- these will be translated into their bytecode equivalents
+data AbstractInstruction 
+    = Ret JVMValue       -- ^ return to the address in the variable
+    | Load JVMValue      -- ^ pushes the variable onto the stack
+    | Store JVMValue     -- ^ pops a value into the local variable
+    | Push JVMValue
+    | Get JVMClass JVMField -- ^ get a field (static or non-static)
+    | Put JVMClass JVMField JVMValue -- ^ put a value into the specified (static or non-static) field
+    
