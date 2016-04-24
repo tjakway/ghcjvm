9f70f8fdf715f7aed8b75e02660fd3b2c30a55bf
Thomas Jakway <tjakway@nyu.edu>
2016-04-23 22:01:03 -0400

    Added skeleton JVM Monad copied from LlvmM

---
+++ compiler/jvmGen/Jvm/JMonad.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE CPP #-}
+
+-- | Jvm Monad
+-- modeled on the LLVM Monad (LlvmM), see LlvmCodeGen.Base
+
+module Jvm.JMonad where
+
+#include "HsVersions.h"
+
+import Jvm.Types
+import BufWrite   ( BufHandle )
+import Control.Monad (ap)
+import DynFlags
+import UniqSupply
+
+-- version of bytecode output, NOT the version of the currently installed
+-- JVM
+type JvmVersion = Float
+
+data JvmEnv = JvmEnv
+    { envVersion :: JvmVersion       -- ^ target JVM version
+    , envDynFlags :: DynFlags        -- ^ Dynamic flags
+    , envOutput :: BufHandle         -- ^ Output buffer
+    , envUniq :: UniqSupply          -- ^ Supply of unique values
+    }
+
+-- | JMonad, wraps state and IO
+newtype JMonad a = JMonad { runJMonad :: JvmEnv -> IO (a, JvmEnv) }
+
+instance Functor JMonad where
+    fmap f m = JMonad $ \env -> do (x, env') <- runJMonad m env
+                                   return (f x, env')
+
+instance Applicative JMonad where
+    pure x = JMonad $ \env -> return (x, env)
+    (<*>) = ap
+
+instance Monad JMonad where
+    m >>= f  = JMonad $ \env -> do (x, env') <- runJMonad m env
+                                   runJMonad (f x) env'
+
+instance HasDynFlags JMonad where
+    getDynFlags = JMonad $ \env -> return (envDynFlags env, env)
