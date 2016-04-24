ce7649acc7aba3b83746138a631ab66b53fe448f
Thomas Jakway <tjakway@nyu.edu>
2016-04-23 21:11:59 -0400

    wrote mkConst

--- compiler/jvmGen/Jvm/CodeGen.hs
+++ compiler/jvmGen/Jvm/CodeGen.hs
@@ -13,3 +13,7 @@ numLocalVars = undefined
 -- | compile an abstract instruction into its concrete type
 compileInstruction :: AbstractInstruction -> Instruction
 compileInstruction = undefined
+
+mkConst :: JVMVar -> JVMVar
+mkConst (JVMVar a []) = JVMVar a [Final]
+mkConst (JVMVar a attrs) = if Final `elem` attrs then (JVMVar a attrs) else (JVM a (Final : attrs))
