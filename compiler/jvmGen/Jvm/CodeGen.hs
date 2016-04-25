0b5d4a8e990d5602818e07094f6f288424117b86
Thomas Jakway <tjakway@nyu.edu>
2016-04-25 14:08:30 -0400

    wrote explanation for resolveBinarySignature stub

--- compiler/jvmGen/Jvm/CodeGen.hs
+++ compiler/jvmGen/Jvm/CodeGen.hs
@@ -17,3 +17,14 @@ compileInstruction = undefined
 mkConst :: JvmVar -> JvmVar
 mkConst (JvmVar a []) = JvmVar a [Final]
 mkConst (JvmVar a attrs) = if Final `elem` attrs then (JvmVar a attrs) else (JvmVar a (Final : attrs))
+
+-- ^ returns instructions for converting from one binary signature to
+-- another
+-- simple example: actual = (([Int], []), _); desired = (_, ([Float], [])); ought to return i2f
+-- this function only evaluates the OUTPUT of _actual_ and the INPUT of
+-- _desired_.  It ignores the first and second inner tuples of those 
+-- respectively
+-- this function is not all-knowing.  It may therefore fail and return
+-- an error message as a Left
+resolveBinarySignatures :: BinarySignature -> BinarySignature -> Either FastString [Instruction]
+resolveBinarySignatures actual desired = undefined
