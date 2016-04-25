b43fe4d483b5db5abf7421dbd782d4e7e00fd137
Thomas Jakway <tjakway@nyu.edu>
2016-04-25 14:44:25 -0400

    added stubs for binary signature methods began writing Jvm.CodeGen.stmtToInstrs

--- compiler/jvmGen/Jvm/CodeGen.hs
+++ compiler/jvmGen/Jvm/CodeGen.hs
@@ -1,9 +1,10 @@
 module Jvm.CodeGen where
 
+import OrdList
 import Jvm.Types
 import Jvm.Expressions
 import Jvm.Instructions
-
+(
 stackSize :: JvmMethod -> Int
 stackSize = undefined
 
@@ -18,6 +19,25 @@ mkConst :: JvmVar -> JvmVar
 mkConst (JvmVar a []) = JvmVar a [Final]
 mkConst (JvmVar a attrs) = if Final `elem` attrs then (JvmVar a attrs) else (JvmVar a (Final : attrs))
 
+-- | walk through each instruction and make sure the types line up
+-- fail with error message if they don't
+safeComputeBinarySignature :: [Instruction] -> Either FastString BinarySignature
+safeComputeBinarySignature = undefined
+
+-- | the fast and scary way
+-- just pull the input from the first instruction and the output from the
+-- last instruction and call it a day
+-- doesn't return Either because it can never fail
+unsafeComputeBinarySignature :: [Instruction] -> BinarySignature
+unsafeComputeBinarySignature [] = emptySignature
+unsafeComputeBinarySignature instrs = (fst . head $ instrs, snd . last $ instrs)
+
+-- | see if the passed binary signature is correct for the passed
+-- instructions
+-- returns Either an error message or the same binary signature
+checkBinarySignature :: [Instruction] -> BinarySignature -> Either FastString BinarySignature
+checkBinarySignature = undefined
+
 -- ^ returns instructions for converting from one binary signature to
 -- another
 -- simple example: actual = (([Int], []), _); desired = (_, ([Float], [])); ought to return i2f
@@ -28,3 +48,16 @@ mkConst (JvmVar a attrs) = if Final `elem` attrs then (JvmVar a attrs) else (Jvm
 -- an error message as a Left
 resolveBinarySignatures :: BinarySignature -> BinarySignature -> Either FastString [Instruction]
 resolveBinarySignatures actual desired = undefined
+
+type CodeBlock = OrdList Instruction
+
+-- | Modeled on the Llvm code generator's function of the same name
+stmtToInstrs :: CmmNode e x -> JMonad CodeBlock
+        
+stmtToInstrs stmt = case stmt of
+
+    -- | keep comments, don't keep ticks or unwind instructions
+    CmmComment s         -> return (Comment s)
+    CmmTick    _         -> return nilOL
+    CmmUnwind  {}        -> return nilOL
+
